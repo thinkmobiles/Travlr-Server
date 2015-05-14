f8f95c25540be702a811a899577837910762e1aa
ol <ol@nxc.no>
2015-05-14 18:08:55 +0300

    tables contact

--- constants/tables.js
+++ constants/tables.js
@@ -1,4 +1,14 @@
 module.exports = {
     IMAGES: 'images',
-    USERS: 'users'
+    USERS: 'users',
+    POSTS: 'posts',
+    FEEDBACKS: 'feedbacks',
+    FEEDBACKS: 'feedbacks',
+    IMAGES: 'images',
+    COMPLAINTS: 'complaints',
+    POST_CATEGORIES: 'post_categories',
+    CITIES: 'cities',
+    COUNTRIES: 'countries',
+    STATIC_INFO: 'static_info'
+
 };
