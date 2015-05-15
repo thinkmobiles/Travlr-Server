1457daa7726163c1693969d780d91b712120e724
ol <ol@nxc.no>
2015-05-15 09:00:16 +0300

    changes

--- config/development.js
+++ config/development.js
@@ -1,4 +1,4 @@
-﻿  process.env.APP_HOST = 'http://localhost:8835';
+﻿process.env.APP_HOST = 'http://localhost:8835';
 process.env.DB_HOST = 'localhost';
 //process.env.DB_HOST = '192.168.88.250';
 process.env.DB_USER = "postgres";
