09d2c5a8f65f0a36dd829e9abe4d8882eecbe674
YuraDerevenko <jedprankster7@gmail.com>
2015-05-15 17:02:07 +0300

    signIn

--- handlers/sessions.js
+++ handlers/sessions.js
@@ -4,37 +4,15 @@ var RESPONSES = require('../constants/responseMessages');
 var Session = function (PostGre) {
 
     this.register = function (req, res, options) {
-        if (req.session && options && req.session.cid === options.cid) {
-            return res.status(200).send({success: RESPONSES.SUCCESSFUL_LOGOUT, cid: options.id});
+        if (req.session && options && req.session.id === options.id) {
+            return res.status(200).send({success: RESPONSES.SUCCESSFUL_LOGIN, id: options.id});
         }
         req.session.loggedIn = true;
-        req.session.cid = options.cid;
+        req.session.userId = options.id;
         req.session.login = options.email;
-        res.status(200).send({success: RESPONSES.SUCCESSFUL_LOGOUT, cid: options.id});
+        res.status(200).send({success: RESPONSES.SUCCESSFUL_LOGIN, id: options.id});
     };
 
-    this.userRegister = function (req, res, options) {
-        //TODO  need use camelCase
-        if (options.MemberNumber) {
-            req.session.Id = options.MemberNumber;
-            req.session.Type = CONSTANTS.SESSION_TYPES.MEMBER
-        } else {
-            req.session.Members = [];
-                options.Members.forEach(function(member){
-                    req.session.Members.push(member.MemberNumber)
-            });
-            req.session.Id = options.ID;
-            req.session.Type = CONSTANTS.SESSION_TYPES.FAMILY
-        }
-        req.session.loggedIn = true;
-        req.session.uId = options.Uid;
-
-        if (req.session.Type === CONSTANTS.SESSION_TYPES.MEMBER) {
-            res.status(200).send({success: RESPONSES.SUCCESSFUL_LOGIN, isFamily: false});
-        } else {
-            res.status(200).send({success: RESPONSES.SUCCESSFUL_LOGIN, isFamily: true});
-        }
-    };
 
     this.kill = function (req, res, next) {
         if (req.session) {
--- handlers/users.js
+++ handlers/users.js
@@ -12,6 +12,7 @@ var UsersHelper = require('../helpers/users');
 Users = function (PostGre) {
     var UserModel = PostGre.Models.users;
     var usersHelper = new UsersHelper(PostGre);
+    var session = new Session(PostGre);
 
     this.signUp = function (req, res, next) {
         var options = req.body;
@@ -34,6 +35,30 @@ Users = function (PostGre) {
         }, {checkFunctions : ['checkUniqueEmail']})
 
 
+    };
+
+    this.signIn = function (req, res, next) {
+        var options = req.body;
+
+        if (options && options.email && options.password) {
+            UserModel
+                .forge({
+                    email: options.email,
+                    password: cryptoPass.getEncryptedPass(options.password)
+                })
+                .fetch()
+                .then(function (user) {
+                    if (user && user.id) {
+                        user = user.toJSON();
+                        session.register(req, res, user)
+                    } else {
+                        res.status(400).send(RESPONSES.INVALID_PARAMETERS)
+                    }
+                })
+                .otherwise(next)
+        } else {
+            res.status(400).send(RESPONSES.INVALID_PARAMETERS)
+        }
     };
 
 
