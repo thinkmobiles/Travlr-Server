7e0bc87b3ffe4270332d14b92af7e06c52b42cd1
ol <ol@nxc.no>
2015-05-15 08:45:35 +0300

    development

---
+++ config/development.js
@@ -0,0 +1,13 @@
+﻿process.env.APP_HOST = 'http://localhost:8835';
+process.env.DB_HOST = 'localhost';
+//process.env.DB_HOST = '192.168.88.250';
+process.env.DB_USER = "postgres";
+process.env.DB_PASS = "postgres";
+process.env.DB_NAME = "travlr_dev";
+process.env.DB_PORT = 5432;
+
+process.env.UPLOADING_TYPE = 'FileSystem';
+
+process.env.REDIS_HOST = '134.249.164.53';
+process.env.REDIS_PORT = 6379;
+
