8ffe6e518e0040f04550a9da14a48bee132cfdfa
YuraDerevenko <jedprankster7@gmail.com>
2015-05-18 16:52:49 +0300

    backbone

--- handlers/users.js
+++ handlers/users.js
@@ -66,6 +66,10 @@ Users = function (PostGre) {
         }
     };
 
+    this.signInViaFB = function (req, res, next) {
+        var options = req.body;
+    };
+
     this.getUserById = function (req, res, next) {
         var userId = req.params.id;
 
