04c600db42973aeeb686baae7fec62da7308852e
Roma <roma.buchuk@gmail.com>
2015-06-17 11:21:06 +0000

    update config file

--- config/development.js
+++ config/development.js
@@ -1,17 +1,15 @@
-﻿process.env.APP_HOST = 'http://192.168.88.21';
-process.env.DB_HOST = '192.168.88.250';
+process.env.APP_HOST = 'http://134.249.164.53:8832';
+process.env.HOST = '134.249.164.53:8832';
 //process.env.DB_HOST = 'localhost';
-//process.env.APP_HOST = 'http://localhost';
-process.env.HOST = 'localhost';
-//process.env.HOST = '192.168.88.84:3035';
+process.env.DB_HOST = '192.168.88.250';
 process.env.DB_USER = "postgres";
 process.env.DB_PASS = "postgres";
 process.env.DB_NAME = "travlr_dev";
 process.env.DB_PORT = 5432;
-process.env.PORT = 3035;
+process.env.PORT = 8832;
 
 process.env.UPLOADING_TYPE = 'FileSystem';
 
-process.env.REDIS_HOST = '134.249.164.53';
+process.env.REDIS_HOST = '192.168.88.250';
 process.env.REDIS_PORT = 6379;
 
