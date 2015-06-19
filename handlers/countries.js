6f06e543bc3a7890f9a7357ced93c5929dcf6ca6
ol <ol@nxc.no>
2015-06-19 17:08:00 +0300

    country

--- handlers/countries.js
+++ handlers/countries.js
@@ -5,86 +5,84 @@ var CONSTANTS = require('../constants/constants');
 var Countries;
 
 Countries = function (PostGre) {
-  var CountryModel = PostGre.Models.countries;
-  var CountryCollection = PostGre.Collections[COLLECTIONS.COUNTRIES];
-  var redisClient = require('../helpers/redisClient')();
-  var date = new Date();
+    var CountryModel = PostGre.Models.countries;
+    var CountryCollection = PostGre.Collections[COLLECTIONS.COUNTRIES];
+    var redisClient = require('../helpers/redisClient')();
+    var date = new Date();
 
-  this.createCountry = function (country, callback) {
-    if (country) {
-      CountryModel
-        .forge({'code': country.code})
-        .fetch()
-        .then(function (countryModel) {
-          if (countryModel && countryModel.id) {
-            callback(null, {'countryId': countryModel.id});
-          } else {
+    this.createCountry = function (country, callback) {
+        if (country) {
             CountryModel
-              .forge({
-                'code': country.code,
-                'name': country.name
-              })
-              .save()
-              .then(function (model) {
-                if (model.id) {
-                  redisClient.cacheStore.writeToStorage(CONSTANTS.REDIS_NAME.COUNTRY, date.valueOf());
-                  callback(null, {'countryId': model.id});
-                } else {
-                  callback(RESPONSES.INTERNAL_ERROR);
-                }
-              })
-              .otherwise(callback);
-          }
-        })
-        .otherwise()
+                .forge({'code': country.code})
+                .fetch()
+                .then(function (countryModel) {
+                    if (countryModel && countryModel.id) {
+                        callback(null, {'countryId': countryModel.id});
+                    } else {
+                        CountryModel
+                            .forge({
+                                'code': country.code,
+                                'name': country.name
+                            })
+                            .save()
+                            .then(function (model) {
+                                if (model.id) {
+                                    redisClient.cacheStore.writeToStorage(CONSTANTS.REDIS_NAME.COUNTRY, date.valueOf());
+                                    callback(null, {'countryId': model.id});
+                                } else {
+                                    callback(RESPONSES.INTERNAL_ERROR);
+                                }
+                            })
+                            .otherwise(callback);
+                    }
+                })
+                .otherwise()
 
-    } else {
-      callback(RESPONSES.NOT_ENOUGH_PARAMETERS);
-    }
-  };
-
-  this.getCountries = function (req, res, next) {
-    var itag = req.query.itag;
-    var orderBy = req.query.orderBy || 'name';
-    var order = req.query.order || 'ASC';
-    var searchTerm = req.query.searchTerm;
-    var redisCountryiTag;
-
-    redisClient.cacheStore.readFromStorage(CONSTANTS.REDIS_NAME.COUNTRY, function (err, resp) {
-      redisCountryiTag = resp;
-      if (itag) {
-        if (itag === redisCountryiTag) {
-          res.status(200).send({
-            posts: [],
-            itag: redisCountryiTag
-          });
-          return;
+        } else {
+            callback(RESPONSES.NOT_ENOUGH_PARAMETERS);
         }
-      }
+    };
 
-      CountryCollection
-        .forge()
-        .query(function (qb) {
-          if (searchTerm) {
-            searchTerm = searchTerm.toLowerCase();
-            qb.whereRaw(
-              "LOWER(body) LIKE '%" + searchTerm + "%' "
-            )
-          }
+    this.getCountries = function (req, res, next) {
+        var itag = req.query.itag;
+        var orderBy = req.query.orderBy || 'name';
+        var order = req.query.order || 'ASC';
+        var searchTerm = req.query.searchTerm;
+        var redisCountryiTag;
 
-          if (orderBy) {
-            qb.orderBy(orderBy, order);
-          }
-        })
-        .fetch()
-        .then(function (countryCollection) {
-          var country = ( countryCollection ) ? countryCollection : [];
-          res.header('itag', redisCountryiTag);
-          res.status(200).send(country);
-        })
-        .otherwise(next);
-    });
-  }
+        redisClient.cacheStore.readFromStorage(CONSTANTS.REDIS_NAME.COUNTRY, function (err, resp) {
+            redisCountryiTag = resp;
+            if (itag) {
+                if (itag === redisCountryiTag) {
+                    res.header('itag', redisCountryiTag);
+                    res.status(200).send([]);
+                    return;
+                }
+            }
+
+            CountryCollection
+                .forge()
+                .query(function (qb) {
+                    if (searchTerm) {
+                        searchTerm = searchTerm.toLowerCase();
+                        qb.whereRaw(
+                            "LOWER(body) LIKE '%" + searchTerm + "%' "
+                        )
+                    }
+
+                    if (orderBy) {
+                        qb.orderBy(orderBy, order);
+                    }
+                })
+                .fetch()
+                .then(function (countryCollection) {
+                    var country = ( countryCollection ) ? countryCollection : [];
+                    res.header('itag', redisCountryiTag);
+                    res.status(200).send(country);
+                })
+                .otherwise(next);
+        });
+    }
 
 };
 
